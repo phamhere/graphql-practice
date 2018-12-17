38085174c4f7d78ddfc4465d4e7dfc4c27640b54
Peter Pham <phamhere@gmail.com>
2018-12-16 20:36:02 -0700

    connected to mlab and added mongoose schemas

--- server/app.js
+++ server/app.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
+const mongoose = require("mongoose");
 
 const schema = require("./schema/schema");
 
 const app = express();
+
+mongoose.connect(
+  "mongodb://peter:password1234@ds155097.mlab.com:55097/graphql-practice",
+  { useNewUrlParser: true }
+);
+mongoose.connection.once("open", () => {
+  console.log("connected to database");
+});
 
 app.use(
   "/graphql",
