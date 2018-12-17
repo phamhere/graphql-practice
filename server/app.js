7eaf6a189d6f49770794b235cdfef01c57d92d70
Peter Pham <phamhere@gmail.com>
2018-12-17 01:18:45 -0700

    rendered booklist

--- server/app.js
+++ server/app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const schema = require("./schema/schema");
 
 const app = express();
+
+app.use(cors());
 
 mongoose.connect(
   "mongodb://peter:password1234@ds155097.mlab.com:55097/graphql-practice",
